8a1039906a9fbda89ccd5a88920d67a27852cb9e
Niranjan Maharana <75609938+niranzanmaharana@users.noreply.github.com>
2022-06-12 13:01:10 +0530

    PhotoAppApiZuulApiGateway

---
+++ PhotoAppApiZuulApiGateway/src/test/java/com/niranzan/photoapp/api/gateway/PhotoAppApiZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niranzan.photoapp.api.gateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
