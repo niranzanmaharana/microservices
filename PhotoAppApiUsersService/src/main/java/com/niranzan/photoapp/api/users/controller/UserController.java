4a1d1fa0fa42eab1345cc1153a98c6d31488d917
Niranjan Maharana <niranjan.maharana@signify.com>
2022-06-22 00:02:42 +0530

    gateway routing and h2 db dependency
    Added gateway routing and also added h2 dependency

--- PhotoAppApiUsersService/src/main/java/com/niranzan/photoapp/api/users/controller/UserController.java
+++ PhotoAppApiUsersService/src/main/java/com/niranzan/photoapp/api/users/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 	@Autowired
 	private Environment environment;
