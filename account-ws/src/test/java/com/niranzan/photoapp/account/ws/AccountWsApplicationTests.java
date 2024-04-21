06f1f1e58175332aa6366ea00fc8f5bd15ed2e48
Niranjan Maharana <niranzan.in@gmail.com>
2024-04-21 13:56:43 +0530

    added discovery-service, api-gateway, account-ws and user-ws

---
+++ account-ws/src/test/java/com/niranzan/photoapp/account/ws/AccountWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niranzan.photoapp.account.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
