bf017454cc7b6931f57ae109e0598996482f52b2
Niranjan Maharana <niranzan.in@gmail.com>
2024-05-01 09:39:04 +0530

    Spring Cloud API Gateway as a Load Balancer

--- user-ws/src/main/java/com/niranzan/photoapp/user/ws/controller/UserController.java
+++ user-ws/src/main/java/com/niranzan/photoapp/user/ws/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.niranzan.photoapp.user.ws.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working";
+        return "Working on port: " + environment.getProperty("local.server.port");
     }
 }
